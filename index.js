e2684492f2a5a97f16636414d1411a43de7ac965
yves <yvesmomplais@hotmail.com>
2021-07-16 19:02:06 -0400

    removed paragraph tags and instead created tags from the getweatherbyzip function

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-
-const cityTag = document.querySelector('#city')
-const weatherTag = document.querySelector('#weather')
-const tempTag = document.querySelector('#temp')
-const feelsLikeTag = document.querySelector('#feelsLike')
-
 const zipcodeSearch = document.querySelector('#zipcode_search')
+
 
 zipcodeSearch.addEventListener('keypress', function (e) {
    if (e.key === 'Enter') {
          const inputVal = document.getElementById("zipcode_search").value;
      
    getWeatherByZip(inputVal) 
+
+
    }
    })
+
--- services.js
+++ services.js
@@ -1,19 +1,32 @@
 function getWeatherByZip(zip) {
+
 const URL = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=3d33cb283c213fe0ce3114d7412d9e0f` 
-{
+
 return fetch(URL)
 .then(res => res.json())
-.then(data => {
+.then(data => { 
+
+    if (inputVal = data.message) {
+    alert("Invalid Zipcode") }
+    
+    else {
     const city = data.name
     const weather = data.weather[0].description
     const temp = data.main.temp
     const feelsLike = data.main.feels_like
-    
-    
-    cityTag.innerText = city
-    weatherTag.innerText = weather
-    tempTag.innerText = temp
-    feelsLikeTag.innerText = feelsLike
- })
+        
+        const newDiv = document.createElement("div");
+      
+        const newContent = document.createTextNode(
+        `The weather in ${city} is ${weather}.
+        The temperature is ${temp} degrees Fahrenheit.
+        It feels like ${feelsLike} degrees Fahrenheit.`);
+      
+        newDiv.appendChild(newContent);
+      
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newDiv, currentDiv);
+
 }
+})
 }

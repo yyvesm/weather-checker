a3b31fe1b2109b8e16b2989f26d00e6ebba5d8d5
yves <yvesmomplais@hotmail.com>
2021-07-16 20:38:38 -0400

    creates element for each piece of data

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const zipcodeSearch = document.querySelector('#zipcode_search')
 
-
 zipcodeSearch.addEventListener('keypress', function (e) {
-   if (e.key === 'Enter') {
-         const inputVal = document.getElementById("zipcode_search").value;
-     
-   getWeatherByZip(inputVal) 
-
-
-   }
-   })
-
+      if (e.key === 'Enter') {
+            const inputVal = document.getElementById("zipcode_search").value;
+            
+            getWeatherByZip(inputVal) 
+            
+            
+      }
+})
--- services.js
+++ services.js
@@ -14,19 +14,23 @@ return fetch(URL)
     const weather = data.weather[0].description
     const temp = data.main.temp
     const feelsLike = data.main.feels_like
-        
-        const newDiv = document.createElement("div");
-      
-        const newContent = document.createTextNode(
-        `The weather in ${city} is ${weather}.
-        The temperature is ${temp} degrees Fahrenheit.
-        It feels like ${feelsLike} degrees Fahrenheit.`);
-      
-        newDiv.appendChild(newContent);
-      
-        const currentDiv = document.getElementById("div1");
-        document.body.insertBefore(newDiv, currentDiv);
 
+        
+       const cityDiv = document.createElement('p');
+       cityDiv.innerText = `The weather in ${city} is:`;
+       document.body.appendChild(cityDiv);
+
+       const weatherDiv = document.createElement('p');
+       weatherDiv.innerText = `${weather}`;
+       document.body.appendChild(weatherDiv);
+        
+       const tempDiv = document.createElement('p');
+       tempDiv.innerText = `The temperature is ${temp} degrees Fahrenheit.`;
+       document.body.appendChild(tempDiv);
+
+       const feelsLikeDiv = document.createElement('p');
+       feelsLikeDiv.innerText = `It feels like ${feelsLike} degrees Fahrenheit.`
+       document.body.appendChild(feelsLikeDiv);
 }
 })
 }
